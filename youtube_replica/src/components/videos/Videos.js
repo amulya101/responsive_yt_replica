a5eaf7d91f3eee9de9a88c71f80058e35da47d50
Amulya Poudel <amulyapoudel8@gmail.com>
2023-04-04 21:17:02 -0400

    Completed replica

---
+++ youtube_replica/src/components/videos/Videos.js
@@ -0,0 +1,124 @@
+import React from 'react'
+import './Videos.css'
+
+export const Videos = () => {
+  return (
+    <div>
+        <input type='text' className='search-bar' placeholder='Search' />
+        
+        <div className="body-grid">
+            <div className="side-menu">
+                under construction
+            </div>
+
+            <div className="video-grid">
+
+                <div className="video-preview">
+                    <div >
+                        <img className='thumbnail' alt='marques and sundar' src='./thumbnail-1.webp'/>
+                    </div>
+                    <div className='desc'>
+                        <div className='channel-img'>
+                            <img className='avi' alt='marques' src='./channel-1.jpeg'/>
+                        </div>
+                        <div className='channel-detail'>
+                            <p className='video-title'>Talking tech and AI with Google CEO Sundar Pichai!</p>
+                            <p className='video-creator'>Marques Brownlee</p>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+
+                <div className="video-preview">
+                    <div >
+                        <img className='thumbnail' alt='marques and sundar' src='./thumbnail-1.webp'/>
+                    </div>
+                    <div className='desc'>
+                        <div className='channel-img'>
+                            <img className='avi' alt='marques' src='./channel-1.jpeg'/>
+                        </div>
+                        <div className='channel-detail'>
+                            <p className='video-title'>Talking tech and AI with Google CEO Sundar Pichai!</p>
+                            <p className='video-creator'>Marques Brownlee</p>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+
+                <div className="video-preview">
+                    <div >
+                        <img className='thumbnail' alt='marques and sundar' src='./thumbnail-1.webp'/>
+                    </div>
+                    <div className='desc'>
+                        <div className='channel-img'>
+                            <img className='avi' alt='marques' src='./channel-1.jpeg'/>
+                        </div>
+                        <div className='channel-detail'>
+                            <p className='video-title'>Talking tech and AI with Google CEO Sundar Pichai!</p>
+                            <p className='video-creator'>Marques Brownlee</p>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+
+                <div className="video-preview">
+                    <div >
+                        <img className='thumbnail' alt='marques and sundar' src='./thumbnail-1.webp'/>
+                    </div>
+                    <div className='desc'>
+                        <div className='channel-img'>
+                            <img className='avi' alt='marques' src='./channel-1.jpeg'/>
+                        </div>
+                        <div className='channel-detail'>
+                            <p className='video-title'>Talking tech and AI with Google CEO Sundar Pichai!</p>
+                            <p className='video-creator'>Marques Brownlee</p>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+
+                <div className="video-preview">
+                    <div >
+                        <img className='thumbnail' alt='marques and sundar' src='./thumbnail-1.webp'/>
+                    </div>
+                    <div className='desc'>
+                        <div className='channel-img'>
+                            <img className='avi' alt='marques' src='./channel-1.jpeg'/>
+                        </div>
+                        <div className='channel-detail'>
+                            <p className='video-title'>Talking tech and AI with Google CEO Sundar Pichai!</p>
+                            <p className='video-creator'>Marques Brownlee</p>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+
+                <div className="video-preview">
+                    <div >
+                        <img className='thumbnail' alt='marques and sundar' src='./thumbnail-1.webp'/>
+                    </div>
+                    <div className='desc'>
+                        <div className='channel-img'>
+                            <img className='avi' alt='marques' src='./channel-1.jpeg'/>
+                        </div>
+                        <div className='channel-detail'>
+                            <p className='video-title'>Talking tech and AI with Google CEO Sundar Pichai!</p>
+                            <p className='video-creator'>Marques Brownlee</p>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+
+            </div>
+
+        </div>
+               
+    </div>
+  )
+}
